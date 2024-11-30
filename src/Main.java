698c83360633b1a9245df98777fa20bba1823f3a
gulyaeva-m-skyeng <marucosmos0924@gmail.com>
2024-11-30 23:56:51 +0300

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // task1
+        System.out.println("Task 1");
+int[] expenses = new int[]{10, 25, 36, 47, 55};
+int totalExpenses = 0;
+
+for (int i = 0; i < expenses.length; i++) {
+    totalExpenses += expenses[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + totalExpenses + " рублей");
     }
 }
