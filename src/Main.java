99c44a5c9e5e3fbd191cac74125d52c38590b71d
gulyaeva-m-skyeng <marucosmos0924@gmail.com>
2024-12-01 02:00:45 +0300

    задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         // task1
@@ -29,5 +31,15 @@ public class Main {
         System.out.println("Сумма трат за месяц составила " + totalExpenses1 + " рублей.");
         System.out.println("Минимальная сумма трат за день составила " + minExpense1 + " рублей.");
         System.out.println("Максимальная сумма трат за день составила " + maxExpense1 + " рублей.");
+
+        // task3
+        System.out.println("Task 3");
+        int[] expenses2 = new int [] {10, 25, 36, 47, 55};
+        int sum = 0;
+        for (int i = 0; i < expenses2.length; i++) {
+            sum += expenses2[i];
+        }
+        double average = (double) sum / expenses2.length;
+        System.out.println(" Средняя сумма трат за месяц составила " + average + " рублей");
     }
 }
